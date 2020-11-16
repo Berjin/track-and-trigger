550e64722076f1f9ce06990ac2b88d42397d543e
Berjin <berjin1298@gmail.com>
2020-11-16 10:59:15 +0530

    Add sign out

--- app/src/main/java/com/track/trackandtrigger/Profile.java
+++ app/src/main/java/com/track/trackandtrigger/Profile.java
@@ -4,16 +4,21 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -38,6 +43,7 @@ public class Profile extends Fragment {
     View share;
     View settings;
     View help;
+    Button sign_out;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -85,6 +91,7 @@ public class Profile extends Fragment {
         share = profileInflater.findViewById(R.id.share_app);
         settings = profileInflater.findViewById(R.id.settings);
         help = profileInflater.findViewById(R.id.help);
+        sign_out = profileInflater.findViewById(R.id.sign_out_btn);
         email_text.setText(user.getEmail());
         name_text.setText(user.getDisplayName());
         Glide.with(this)
@@ -108,6 +115,13 @@ public class Profile extends Fragment {
             Intent urlIntent = new Intent(Intent.ACTION_VIEW);
             urlIntent.setData(Uri.parse(url));
             startActivity(urlIntent);
+        });
+        sign_out.setOnClickListener(v -> {
+            AuthUI.getInstance()
+                    .signOut(getActivity())
+                    .addOnCompleteListener(task -> {
+                        startActivity(new Intent(getActivity(),SplashScreenActivity.class));
+                    });
         });
         // Inflate the layout for this fragment
         return profileInflater;
