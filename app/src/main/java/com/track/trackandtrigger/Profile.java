692181fb27a4d2e63f400b1c9f44674cf6a5c7eb
Berjin <berjin1298@gmail.com>
2020-11-16 10:37:10 +0530

    Add share, modify profile

--- app/src/main/java/com/track/trackandtrigger/Profile.java
+++ app/src/main/java/com/track/trackandtrigger/Profile.java
@@ -1,12 +1,22 @@
 package com.track.trackandtrigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,6 +30,13 @@ public class Profile extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    TextView email_text;
+    TextView name_text;
+    CircleImageView profile_dp;
+    View personal_info;
+    View share;
+    View settings;
+    View help;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -58,7 +75,37 @@ public class Profile extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View profileInflater = inflater.inflate(R.layout.fragment_profile, container, false);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        email_text = profileInflater.findViewById(R.id.email_text);
+        name_text = profileInflater.findViewById(R.id.name_text);
+        profile_dp = profileInflater.findViewById(R.id.profile_pic);
+        personal_info = profileInflater.findViewById(R.id.personal_info);
+        share = profileInflater.findViewById(R.id.share_app);
+        settings = profileInflater.findViewById(R.id.settings);
+        help = profileInflater.findViewById(R.id.help);
+        email_text.setText(user.getEmail());
+        name_text.setText(user.getDisplayName());
+        Glide.with(this)
+                .load(user.getPhotoUrl())
+                .into(profile_dp);
+        personal_info.setOnClickListener(v -> {
+            Toast.makeText(getActivity(), "Profile", Toast.LENGTH_SHORT).show();
+        });
+        share.setOnClickListener(v -> {
+            Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+            sharingIntent.setType("text/plain");
+            String shareBody = "*Track and Trigger*\nHey there!\nTrack and Trigger helps you keep track of your day to day needs. I find this app very useful. \nGet this from https://example.com";
+            sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT,shareBody);
+            startActivity(Intent.createChooser(sharingIntent,"Share via"));
+        });
+        settings.setOnClickListener(v -> {
+            Toast.makeText(getActivity(), "Settings", Toast.LENGTH_SHORT).show();
+        });
+        help.setOnClickListener(v -> {
+            Toast.makeText(getActivity(), "Help", Toast.LENGTH_SHORT).show();
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return profileInflater;
     }
 }
