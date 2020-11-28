dc217e443db213b5413a669b5b4566aae1dcdcde
Berjin <berjin1298@gmail.com>
2020-11-28 11:51:34 +0530

    add items view starter

--- app/src/main/java/com/track/trackandtrigger/ItemRecyclerviewAdapter.java
+++ app/src/main/java/com/track/trackandtrigger/ItemRecyclerviewAdapter.java
@@ -1,5 +1,6 @@
 package com.track.trackandtrigger;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.PopupMenu;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
@@ -38,17 +40,26 @@ public class ItemRecyclerviewAdapter extends FirebaseRecyclerAdapter<ItemsModel,
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView textItem;
         ImageView category_popup;
+        CardView item_card_view;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             textItem =itemView.findViewById(R.id.textItem);
             category_popup = itemView.findViewById(R.id.category_popup);
+            item_card_view = itemView.findViewById(R.id.item_card_view);
             category_popup.setOnClickListener(this);
+            item_card_view.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View v) {
-            showPopupMenu(v);
+            switch (v.getId()){
+                case R.id.category_popup:
+                    showPopupMenu(v);
+                    break;
+                case R.id.item_card_view:
+//                    Intent intent =
+            }
         }
         private void showPopupMenu(View view){
             PopupMenu popupMenu = new PopupMenu(view.getContext(),view);
